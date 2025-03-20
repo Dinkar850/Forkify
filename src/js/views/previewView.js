79a736c9654436017916d4aeb3930628a273f291
Dinkar850 <guptadinkar850@gmail.com>
2025-03-20 21:08:00 +0530

    fixed error

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -3,7 +3,8 @@ import icons from 'url:../../img/icons.svg';
 // const icons = 'static/icons.svg';
 // import icons from 'static/icons.svg';
 // const icons = new URL('../../img/icons.svg', import.meta.url);
-const icons = './static/icons.svg';
+// const icons = './static/icons.svg';
+import icons from 'url:../../img/icons.svg';
 
 class PreviewView extends View {
   _parentElement = '';
